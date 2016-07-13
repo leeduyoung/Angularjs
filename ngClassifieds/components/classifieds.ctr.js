9f9d06a77f4d8f210b164564a47c8bff42241699
이두영 <leeduyoung20@naver.com>
2016-07-14 00:35:00 +0900

    $http Study * Working with Remote Data * Http Requests and Promises * Switching to Services
    CRUD and User Interface Refinement Study
    * Putting in the Sidenav
    * Adding New Classifieds

--- ngClassifieds/components/classifieds.ctr.js
+++ ngClassifieds/components/classifieds.ctr.js
@@ -4,14 +4,49 @@
 
     angular
         .module("ngClassifieds")
-        .controller("classifiedsCtrl", function($scope) {
+        .controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
 
-            $scope.name = {
-                first: "Duyoung",
-                last: "Lee"
-            };
+            // $http.get('data/classifieds.json').then(function(classifieds) {
+            //     console.log(classifieds);
+            //     $scope.classifieds = classifieds.data;
+            // });
 
-            $scope.message = "Hello, world!!";
+            classifiedsFactory.getClassifieds().then(function(classifieds) {
+                $scope.classifieds = classifieds.data;
+            });
+
+            var contact = {
+                name : "Duyoung Lee",
+                phone : "(031) 283-1476",
+                email : "leeduyoung@naver.com"
+            }
+
+            $scope.openSidebar = function() {
+                $mdSidenav('left').open();
+            }
+
+            $scope.closeSidebar = function() {
+                $mdSidenav('left').close();
+            }
+
+            $scope.saveClassified = function(classified) {
+                console.log(classified);
+                if(classified) {
+                    classified.contact = contact;
+                    $scope.classifieds.push(classified);
+                    $scope.classified = {};
+                    $scope.closeSidebar();
+                    $mdToast.show(
+                        $mdToast.simple()
+                            .content("Classified Saved!")
+                            .position('top, right')
+                            .hideDelay(3000)
+                    );
+                }                
+            }
+
+            $scope.saveEdit = function() {
+                
+            }
         });
-
 })();
